ece9a1ebc59f64d037814a1c4d8b3f6bf1642e4e
shivam satpute <shivam897satpute@gmail.com>
2021-05-11 15:55:20 +0530

    [shivam]add: Contact Details

---
+++ AddressBook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Program");
+            Console.Write("Enter First Name:- ");  
+            string firstName = Console.ReadLine(); 
+            Console.Write("Enter Last Name:- ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Address:- ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City:- ");
+            string city = Console.ReadLine();
+            Console.Write("Enter State:- ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Zip Code :- ");
+            string zip = Console.ReadLine();
+            Console.Write("Enter Phone Number:- ");
+            string pNumber = Console.ReadLine();
+            Console.Write("Enter Email:- ");
+            string Email = Console.ReadLine();
+        }
+    }
+}
+
